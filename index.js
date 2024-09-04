e76047b87f5b8e4eb6a284909b7facb9516e1c45
Olya <olachobanuk@gmail.com>
2024-09-04 11:55:39 +0200

    Lesson1++

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const {fooHelper} = require('./helper');
 const http = require('node:http');
 const path = require('node:path');
+const os = require('os');
 const readline = require('node:readline');
+const fs = require('fs');
 const foo = async () =>{
     //http
     // const server = http.createServer((req, res) => {
@@ -25,4 +27,25 @@ const foo = async () =>{
     //
     // rlInstance.question('Name?')
 }
-void foo();
+// void foo();
+
+//PATH
+// const fullPathName = path.join(__dirname,'test','helper.js');
+// console.log(fullPathName);
+
+
+//OS
+// console.log(os.arch());
+// console.log(os.cpus());
+
+
+//FS
+
+fs.readFile(path.join(__dirname,'text.txt'),{encoding:"utf-8"},(err, data) =>{
+    if(err) throw new Error();
+    console.log(data);
+});
+
+fs.writeFile(path.join(__dirname,'text2.txt'),'Hello from Okten',(err) =>{
+    if(err) throw new Error();
+})
